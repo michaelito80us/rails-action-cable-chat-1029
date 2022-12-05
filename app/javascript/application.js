f6aac7c847b1da3581382e7c3edea20d642f59ca
Michael Epelboim <sdrmike@outlook.com>
2022-12-05 08:47:37 +0800

    added import of JS function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,8 @@
-import "@hotwired/turbo-rails"
-import 'bootstrap';
+import "@hotwired/turbo-rails";
+import "bootstrap";
+
+// import {initChatroomCable} from "./components/chatroom_subscriptions";
+
+// document.addEventListener("turbo:load", () => {
+//   initChatroomCable();
+// })
